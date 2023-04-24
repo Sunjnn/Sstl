ce0b3de8a11be52255ae4bc2bd6bd80952b6e6f9
sunjnn <sunjn990@gmail.com>
2023-04-24 17:08:35 +0800

    feat: add distance functions for different iterator types in Sstl

--- Sstl/stl_iterator.h
+++ Sstl/stl_iterator.h
@@ -76,4 +76,32 @@ value_type(const Iterator&) {
 
 }
 
+namespace Sstl {
+
+template<class InputIterator>
+inline typename iterator_traits<InputIterator>::difference_type
+__distance(InputIterator first, InputIterator last, input_iterator_tag) {
+    iterator_traits<InputIterator>::difference_type n = 0;
+    while (first != last) {
+        ++first;
+        ++n;
+    }
+    return n;
+}
+
+template<class RandomAccessIterator>
+inline typename iterator_traits<RandomAccessIterator>::difference_type
+__distance(RandomAccessIterator first, RandomAccessIterator last, random_access_iterator_tag) {
+    return last - first;
+}
+
+template<class InputIterator>
+inline typename iterator_traits<InputIterator>::difference_type
+distance(InputIterator first, InputIterator last) {
+    typedef iterator_traits<InputIterator>::iterator_category category;
+    return __distance(first, last, category());
+}
+
+}
+
 #endif

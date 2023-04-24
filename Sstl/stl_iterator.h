e647fa457714a5f2e60f58d2741078fcb2241af9
sunjnn <sunjn990@gmail.com>
2023-04-24 17:04:14 +0800

    feat: add iterator helper functions to Sstl/stl_iterator.h

--- Sstl/stl_iterator.h
+++ Sstl/stl_iterator.h
@@ -53,4 +53,27 @@ struct iterator_traits<const T*> {
 
 }
 
+namespace Sstl {
+
+template<class Iterator>
+inline typename iterator_traits<Iterator>::iterator_category
+iterator_category(const Iterator&) {
+    typedef typename iterator_traits<Iterator>::iterator_category category;
+    return category();
+}
+
+template<class Iterator>
+inline typename iterator_traits<Iterator>::difference_type*
+distance_type(const Iterator&) {
+    return static_cast<typename iterator_traits<Iterator>::difference_type*>(0);
+}
+
+template<class Iterator>
+inline typename iterator_traits<Iterator>::value_type*
+value_type(const Iterator&) {
+    return static_cast<typename iterator_traits<Iterator>::value_type*>(0);
+}
+
+}
+
 #endif

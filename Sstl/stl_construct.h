5d5ca1016fa88ceeb30511dfd6b5bf992c014ca0
sunjnn <sunjn990@gmail.com>
2023-04-27 10:12:51 +0800

    fix: refactor some errs in `Sstl::destroy()` and `Sstl::__destroy()` function

--- Sstl/stl_construct.h
+++ Sstl/stl_construct.h
@@ -2,6 +2,7 @@
 #define __STL_CONSTRUCT__H__
 
 #include "type_traits.h"
+#include "stl_iterator.h"
 
 namespace Sstl {
 
@@ -16,19 +17,6 @@ inline void destroy(T *pointer) {
     pointer->~T();
 }
 
-// second version of destroy, accept a range
-template<class ForwardIterator>
-inline void destroy(ForwardIterator first, ForwardIterator last) {
-    __destroy(first, last, value_type(first));
-}
-
-// if there is trivial destructor
-template<class ForwardIterator, class T>
-inline void __destroy(ForwardIterator first, ForwardIterator last, T*) {
-    typedef typename __type_traits<T>::has_trivial_destructor trivial_destructor;
-    __destroy__aux(first, last, trivial_destructor());
-}
-
 // no trivial destructor
 // call first version of destroy for each element
 template<class ForwardIterator>
@@ -40,7 +28,24 @@ inline void __destroy_aux(ForwardIterator first, ForwardIterator last, __false_t
 
 // there is trivial destructor
 template<class ForwardIterator>
-inline void __destroy_aux(ForwardIterator, ForwardIterator, __true_type) {}
+inline void __destroy_aux(ForwardIterator first, ForwardIterator last, __true_type) {
+    for (; first < last; ++first) {
+        destroy(&*first);
+    }
+}
+
+// if there is trivial destructor
+template<class ForwardIterator, class T>
+inline void __destroy(ForwardIterator first, ForwardIterator last, T*) {
+    typedef typename __type_traits<T>::has_trivial_destructor trivial_destructor;
+    __destroy_aux(first, last, trivial_destructor());
+}
+
+// second version of destroy, accept a range
+template<class ForwardIterator>
+inline void destroy(ForwardIterator first, ForwardIterator last) {
+    __destroy(first, last, value_type(first));
+}
 
 inline void destroy(char*, char*) {}
 inline void destroy(wchar_t*, wchar_t*){}

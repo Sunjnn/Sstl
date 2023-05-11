e220d92909bd9cc6b45ff1ccedb87df853de5bb1
sunjnn <sunjn990@gmail.com>
2023-05-11 16:41:40 +0800

    test: add deque constructor deque(int n, const value_type &value) tests to test suite

--- test/test_deque.cpp
+++ test/test_deque.cpp
@@ -1,5 +1,6 @@
 // my implementation
 #include "stl_deque.h"
+#include "test.h"
 
 // gcc implementation
 #include <iostream>
@@ -15,6 +16,22 @@ int main(int argc, char **argv) {
     assert(deque0.size() == 0);
     assert(deque0.max_size() == 8 * 512 / 4);
     assert(deque0.empty() == true);
+    cout << "success!\n" << endl;
+
+    // construct deque(int n, const value_type &value)
+    cout << "********************" << endl;
+    cout << "test constructor" << endl;
+    Sstl::deque<int> deque1(20 * 128, 10);
+    assert(compare_n(deque1, 20 * 128, 10));
+    assert(deque1.size() == 20 * 128);
+    assert(deque1.max_size() == 23 * 128);
+    assert(deque1.empty() == false);
+
+    Sstl::deque<int> deque2(12, 10);
+    assert(compare_n(deque2, 12, 10));
+    assert(deque2.size() == 12);
+    assert(deque2.max_size() == 8 * 128);
+    assert(deque2.empty() == false);
     cout << "success!\n" << endl;
 
     return 0;

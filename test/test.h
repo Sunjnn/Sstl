f7b9a6941fa799caff5651d5a1d40eda97d5b205
sunjnn <sunjn990@gmail.com>
2023-05-12 16:30:59 +0800

    feat: add compare_n function overload for initializer_list

--- test/test.h
+++ test/test.h
@@ -1,6 +1,8 @@
 #ifndef __TEST_H__
 #define __TEST_H__
 
+#include "stl_initializer_list.h"
+
 #include <cstddef>
 
 template<class ForwardIterator, class T>
@@ -17,6 +19,21 @@ bool compare_n(ForwardIterator first, ForwardIterator last, const size_t n, cons
 template<class container, class T>
 bool compare_n(container con, const size_t n, const T &x) {
     return compare_n(con.begin(), con.end(), n, x);
+}
+
+template<class ForwardIterator, class T>
+bool compare_n(ForwardIterator first, ForwardIterator last, std::initializer_list<T> il) {
+    typedef typename std::initializer_list<T>::iterator ilitT;
+    ilitT it_il = il.begin();
+    ilitT it_il_end = il.end();
+
+    while (first < last && it_il < it_il_end) {
+        if (*first != *it_il) return false;
+        ++first;
+        ++it_il;
+    }
+
+    return first == last && it_il == it_il_end;
 }
 
 #endif

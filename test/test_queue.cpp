b1ddf332781dbf0a52a8d06cc8e36b20372cd6aa
sunjnn <sunjn990@gmail.com>
2023-05-15 18:33:55 +0800

    test: test `push()`, `pop()`, `front()`, and `back()` with assertions

--- test/test_queue.cpp
+++ test/test_queue.cpp
@@ -13,5 +13,25 @@ int main() {
     assert(queue0.size() == 0);
     cout << "success!\n" << endl;
 
+    // push(), pop(), front() and back()
+    cout << "********************" << endl;
+    cout << "test push(), pop(), front() and back()" << endl;
+    for (int i = 0; i < 10; ++i) {
+        queue0.push(i);
+        assert(queue0.front() == 0);
+        assert(queue0.back() == i);
+    }
+    assert(queue0.empty() == false);
+    assert(queue0.size() == 10);
+    for (int i = 1; i < 10; ++i) {
+        queue0.pop();
+        assert(queue0.front() == i);
+        assert(queue0.back() == 9);
+    }
+    queue0.pop();
+    assert(queue0.empty() == true);
+    assert(queue0.size() == 0);
+    cout << "success!\n" << endl;
+
     return 0;
 }

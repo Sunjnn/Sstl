242de6cff4167255ff94a835286046e1de3a7d04
sunjnn <sunjn990@gmail.com>
2023-05-15 17:54:09 +0800

    test: test `push()`, `top()`, and `pop()` functions for Stack class

--- test/test_stack.cpp
+++ test/test_stack.cpp
@@ -13,5 +13,23 @@ int main() {
     assert(stack0.size() == 0);
     cout << "success!\n" << endl;
 
+    // push(), top() and pop()
+    cout << "********************" << endl;
+    cout << "test push(), top() and pop()" << endl;
+    for (int i = 0; i < 10; ++i) {
+        stack0.push(i);
+    }
+    assert(stack0.empty() == false);
+    assert(stack0.size() == 10);
+    for (int i = 9; i >= 0; --i) {
+        int x = stack0.top();
+        assert(x == i);
+        stack0.pop();
+        assert(stack0.size() == i);
+    }
+    assert(stack0.empty() == true);
+    assert(stack0.size() == 0);
+    cout << "success!\n" << endl;
+
     return 0;
 }

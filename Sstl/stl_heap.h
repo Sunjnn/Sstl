a49bb056c1d9d70f5c49bedb3ac7f980e075a3e3
sunjnn <sunjn990@gmail.com>
2023-05-17 18:46:04 +0800

    feat: implement pop function of heap in Sstl namespace

--- Sstl/stl_heap.h
+++ Sstl/stl_heap.h
@@ -31,4 +31,44 @@ inline void push_heap(RandomAccessIterator first, RandomAccessIterator last) {
 
 } // namespace Sstl
 
+// define pop function of heap
+namespace Sstl {
+
+template<class RandomAccessIterator, class Distance, class T>
+void __adjust_heap(RandomAccessIterator first, Distance holeIndex, Distance len, T value) {
+    Distance topIndex = holeIndex;
+    Distance secondChild = 2 * holeIndex + 2;
+    while (secondChild < len) {
+        if (*(first + secondChild) < *(first + (secondChild - 1))) --secondChild;
+        *(first + holeIndex) = *(first + secondChild);
+        holeIndex = secondChild;
+        secondChild = 2 * (secondChild - 1);
+    }
+
+    if (secondChild == len) {
+        *(first + holeIndex) = *(first + (secondChild + 1));
+        holeIndex = secondChild - 1;
+    }
+
+    __push_heap(first, holeIndex, topIndex, value);
+}
+
+template<class RandomAccessIterator, class T, class Distance>
+inline void __pop_heap(RandomAccessIterator first, RandomAccessIterator last, RandomAccessIterator result, T value, Distance*) {
+    *result = *first;
+    __adjust_heap(first, distance(0), distance(last - first), value);
+}
+
+template<class RandomAccessIterator, class T>
+inline void __pop_heap_aux(RandomAccessIterator first, RandomAccessIterator last, T*) {
+    __pop_heap(first, last - 1, last - 1, T(*(last - 1)), distance_type(first));
+}
+
+template<class RandomAccessIterator>
+inline void pop_heap(RandomAccessIterator first, RandomAccessIterator last) {
+    __pop_heap_aux(first, last, value_type(first));
+}
+
+} // namespace Sstl
+
 #endif

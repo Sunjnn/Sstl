533dcafad0c49b653bcee67341132a1ba4265e69
sunjnn <sunjn990@gmail.com>
2023-05-22 17:42:10 +0800

    feat: add Sstl's priority_queue class implementation

---
+++ Sstl/stl_priority_queue.h
@@ -0,0 +1,43 @@
+#ifndef __STL_PRIORITY_QUEUE_H__
+#define __STL_PRIORITY_QUEUE_H__
+
+#include "stl_vector.h"
+
+namespace Sstl {
+
+// template<class T, class Sequence = vector<T>, class Compare = less<typename Sequence::value_type>>
+template<class T, class Sequence = vector<T>>
+class priority_queue {
+public:
+    typedef typename Sequence::value_type value_type;
+    typedef typename Sequence::size_type size_type;
+    typedef typename Sequence::reference reference;
+    // typedef typename Sequence::const_reference
+
+protected:
+    Sequence c;
+    // Compare comp;
+
+public:
+    priority_queue() : c() {}
+    // explicit priority_queue(const Compare &x)
+
+    template<class InputIterator>
+    priority_queue(InputIterator first, InputIterator last) : c(first, last) {make_heap(c.begin(), c.end());}
+
+    bool empty() const {return c.empty();}
+    size_type size() const {return c.size();}
+    void push(const value_type &x) {
+        c.push_back(x);
+        push_heap(c.begin(), c.end());
+    }
+    void pop() {
+        pop_heap(c.begin(), c.end());
+        c.pop_back();
+    }
+
+}; // class priority_queue
+
+} // namespace Sstl 
+
+#endif

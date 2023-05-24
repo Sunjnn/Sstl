cbaf1e5f9c2afcfd98677b9a7909b12885b89118
sunjnn <sunjn990@gmail.com>
2023-05-24 18:18:03 +0800

    feat: add slist class with `push_front()` and `pop_front()` methods for singly linked list

--- Sstl/stl_slist.h
+++ Sstl/stl_slist.h
@@ -1,6 +1,8 @@
 #ifndef __STL_SLIST_H__
 #define __STL_SLIST_H__
 
+#include "stl_alloc.h"
+#include "stl_construct.h"
 #include "stl_iterator.h"
 
 #include <cstddef>
@@ -80,6 +82,75 @@ struct __slist_iterator : public __slist_iterator_base {
         return tmp;
     }
 };
+
+template<class T, class Alloc = alloc>
+class slist {
+public:
+    typedef T                   value_type;
+    typedef value_type*         pointer;
+    typedef const value_type*   const_pointer;
+    typedef value_type&         reference;
+    typedef const value_type&   const_reference;
+    typedef size_t              size_type;
+    typedef ptrdiff_t           difference_type;
+
+    typedef __slist_iterator<T, T&, T*>             iterator;
+    typedef __slist_iterator<T, const T&, const T*> const_iterator;
+
+private:
+    typedef __slist_node<T>         list_node;
+    typedef __slist_node_base       list_node_base;
+    typedef __slist_iterator_base   iterator_base;
+
+    typedef simple_alloc<list_node, Alloc> list_node_allocator;
+
+    static list_node* create_node(const value_type &x) {
+        list_node *node = list_node_allocator::allocate();
+        try {
+            construct(&node->data, x);
+            node->next = 0;
+        }
+        catch (...) {
+            list_node_allocator::deallocate(node);
+            return node;
+        }
+    }
+
+    static void destroy(list_node *node) {
+        destroy(&node->data);
+        list_node_allocator::deallocate(node);
+    }
+
+    list_node_base head;
+
+public:
+    slist() {head.next = 0;}
+    ~slist() {clear();}
+
+    iterator begin() {return iterator((list_node*)head.next);}
+    iterator end() {return iterator(0);}
+    size_type size() {return __slist_size(head.next);}
+    bool empty() {return head.next == 0;}
+
+    void swap(slist &L) {
+        list_node_base *tmp = head.next;
+        head.next = L.head.next;
+        L.head.next = tmp;
+    }
+
+    reference front() {return ((list_node*)head.next)->data;}
+
+    void push_front(const value_type &x) {
+        __slist_make_link(&head, create_node(x));
+    }
+
+    void pop_front() {
+        list_node *node = (list_node*)head.next;
+        head.next = node->next;
+        destroy(node);
+    }
+
+}; // class slist
 
 } // namespace Sstl
 

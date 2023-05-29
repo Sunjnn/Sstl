56bdb5ecb15883c316995d92c9d17b3805a1592b
sunjnn <sunjn990@gmail.com>
2023-05-29 18:57:28 +0800

    feat: add red-black tree basic structure to Sstl

---
+++ Sstl/stl_rb_tree.h
@@ -0,0 +1,22 @@
+#ifndef __STL_RB_TREE_H__
+#define __STL_RB_TREE_H__
+
+namespace Sstl {
+
+typedef bool __rb_tree_color_type;
+const __rb_tree_color_type __rb_tree_red = false;
+const __rb_tree_color_type __rb_tree_black = true;
+
+struct __rb_tree_node_base {
+    typedef __rb_tree_color_type color_type;
+    typedef __rb_tree_node_base* base_ptr;
+
+    color_type color;
+    base_ptr parent;
+    base_ptr left;
+    base_ptr right;
+};
+
+}
+
+#endif

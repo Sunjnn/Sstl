7223625e493e14b7e7182df1db0c45d25ee323df
sunjnn <sunjn990@gmail.com>
2023-05-30 18:20:12 +0800

    feat: implement __rb_tree_base_iterator and `increment` and `decrement` functions

--- Sstl/stl_rb_tree.h
+++ Sstl/stl_rb_tree.h
@@ -1,6 +1,12 @@
 #ifndef __STL_RB_TREE_H__
 #define __STL_RB_TREE_H__
 
+// my implementation
+#include "stl_iterator.h"
+
+// gcc implementation
+#include <cstddef>
+
 // rb tree node
 namespace Sstl {
 
@@ -35,6 +41,57 @@ struct __rb_tree_node : public __rb_tree_node_base {
     Value value_field;
 };
 
-}
+} // namespace Sstl
+
+namespace Sstl {
+
+struct __rb_tree_base_iterator {
+    typedef __rb_tree_node_base::base_ptr   base_ptr;
+    typedef bidirectional_iterator_tag      iterator_category;
+    typedef ptrdiff_t                       difference_type;
+
+    base_ptr node;
+
+    // implement operator++
+    void increment() {
+        if (node->right) {
+            node = node->right;
+            while (node->left)
+                node = node->left;
+        }
+        else {
+            base_ptr y = node->parent;
+            while (node == y->right) {
+                node = y;
+                y = node->parent;
+            }
+            if (node->right != y)
+                node = y;
+        }
+    }
+
+    // implement operator--
+    void decrement() {
+        if (node->color == __rb_tree_red && node->parent->parent == node) {
+            node = node->right;
+        }
+        else if (node->left) {
+            base_ptr y = node->left;
+            while (y->right)
+                y = y->right;
+            node = y;
+        }
+        else {
+            base_ptr y = node->parent;
+            while (node == y->left) {
+                node = y;
+                y = y->parent;
+            }
+            node = y;
+        }
+    }
+}; // struct __rb_tree_base_iterator
+
+} // namespace Sstl
 
 #endif

39ffa0b6edb1ddd1702466e02e8b5436650638d4
sunjnn <sunjn990@gmail.com>
2023-06-16 18:31:04 +0800

    feat: add class `pair`

---
+++ Sstl/stl_pair.h
@@ -0,0 +1,21 @@
+#ifndef __STL_PAIR__
+#define __STL_PAIR__
+
+namespace Sstl {
+
+template<class T1, class T2>
+struct pair {
+    typedef T1 first_type;
+    typedef T2 second_type;
+
+    T1 first;
+    T2 second;
+
+    pair() : first(T1()), second(T2()) {}
+    pair(const T1 &a, const T2 &b) : first(a), second(b) {}
+}; // class pair
+
+} // namespace Sstl
+
+
+#endif

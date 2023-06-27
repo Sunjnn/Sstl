8022e7aaf08332f400d128e3389b4de78db3aa06
sunjnn <sunjn990@gmail.com>
2023-06-27 19:29:56 +0800

    test: add test case of rb tree

--- test/test_rb_tree.cpp
+++ test/test_rb_tree.cpp
@@ -13,6 +13,14 @@ struct identity {
     const T& operator()(const T& x) const {return x;}
 };
 
+void compare_whole_tree(Sstl::rb_tree<int, int, identity<int>, std::less<int>> &itree, const string &s) {
+    stringstream strstream;
+    for (auto ite1 = itree.begin(); ite1 != itree.end(); ++ite1) {
+        strstream << *ite1 << "(" << ite1.node->color << ")";
+    }
+    assert(s == strstream.str());
+}
+
 class testRBTree : public testBase {
     virtual void test_func() {
         Sstl::rb_tree<int, int, identity<int>, std::less<int>> itree;
@@ -35,13 +43,21 @@ class testRBTree : public testBase {
         auto ite2 = itree.end();
         Sstl::__rb_tree_base_iterator rbtite;
 
-        string s1 = "5(0)6(1)7(0)8(1)10(1)11(0)12(0)13(1)15(0)";
-        stringstream strstream;
-        for (; ite1 != ite2; ++ite1) {
-            rbtite = Sstl::__rb_tree_base_iterator(ite1);
-            strstream << *ite1 << "(" << rbtite.node->color << ")";
-        }
-        assert(s1 == strstream.str());
+        // red      0
+        // black    1
+        compare_whole_tree(itree, "5(0)6(1)7(0)8(1)10(1)11(0)12(0)13(1)15(0)");
+
+        itree.erase(10);
+        assert(itree.size() == 8);
+        compare_whole_tree(itree, "5(0)6(1)7(0)8(1)11(1)12(0)13(0)15(1)");
+
+        itree.erase(8);
+        assert(itree.size() == 7);
+        compare_whole_tree(itree, "5(0)6(1)7(0)11(1)12(1)13(0)15(1)");
+
+        itree.erase(11);
+        assert(itree.size() == 6);
+        compare_whole_tree(itree, "5(0)6(1)7(0)12(1)13(1)15(0)");
     }
 
 public:

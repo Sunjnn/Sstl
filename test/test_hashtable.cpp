958628e01dd09d8433a4a75b524e399a845183fc
Sunjnn <sunjn990@gmail.com>
2023-07-03 18:57:29 +0800

    test: add test case of hash table

--- test/test_hashtable.cpp
+++ test/test_hashtable.cpp
@@ -46,6 +46,9 @@ class testHashtable : public testBase {
         // }
 
         assert(compare_n(iht.begin(), iht.end(), {0, 1, 2, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 108, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 53, 55, 59, 63}));
+
+        assert(*(iht.find(2)) == 2);
+        assert(iht.count(2) == 2);
     }
 public:
     testHashtable(std::string s) : testBase(s) {}

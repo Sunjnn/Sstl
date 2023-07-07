e2c4ce4e844adc350868f64de13eda96088ca4a4
Sunjnn <sunjn990@gmail.com>
2023-07-07 20:31:01 +0800

    feat: add algorithm function `adjacent_difference()`

--- Sstl/stl_numeric.h
+++ Sstl/stl_numeric.h
@@ -1,6 +1,8 @@
 #ifndef __STL_NUMERIC_H__
 #define __STL_NUMERIC_H__
 
+#include "stl_iterator.h"
+
 // accumulate
 namespace Sstl {
 
@@ -18,6 +20,54 @@ T accumulate(InputIterator first, InputIterator last, T init, BinaryOperation bi
     for (; first != last; ++first)
         init += binary_op(init, *first);
     return init;
+}
+
+} // namespace Sstl
+
+// adjacent difference
+namespace Sstl {
+
+// first version of two of adjacent difference
+template<class InputIterator, class OutputIterator>
+OutputIterator adjacent_difference(InputIterator first, InputIterator last, OutputIterator result) {
+    template<class InputIterator, class OutputIterator, class T>
+    OutputIterator __adjacent_difference(InputIterator first, InputIterator last, OutputIterator result, T*);
+
+    if (first == last) return result;
+    *result = *first;
+    return __adjacent_difference(first, last, result, value_type(first));
+}
+
+// second version of two of adjacent difference
+template<class InputIterator, class OutputIterator, class BinaryOperation>
+OutputIterator adjacent_difference(InputIterator first, InputIterator last, OutputIterator result, BinaryOperation binary_op) {
+    template<class InputIterator, class OutputIterator, class T, class BinaryOperation>
+    OutputIterator __adjacent_difference(InputIterator first, InputIterator last, OutputIterator result, T*, BinaryOperation binary_op);
+    if (first == last) return result;
+    *result = *first;
+    return __adjacent_difference(first, last, result, value_type(first), binary_op);
+}
+
+template<class InputIterator, class OutputIterator, class T>
+OutputIterator __adjacent_difference(InputIterator first, InputIterator last, OutputIterator result, T*) {
+    T value = *first;
+    while (++first != last) {
+        T tmp = *first;
+        *++result = tmp - value;
+        value = tmp;
+    }
+    return ++result;
+}
+
+template<class InputIterator, class OutputIterator, class T, class BinaryOperation>
+OutputIterator __adjacent_difference(InputIterator first, InputIterator last, OutputIterator result, T*, BinaryOperation binary_op) {
+    T value = *first;
+    while (++first != last) {
+        T tmp = *first;
+        *++result = binary_op(tmp, value);
+        value = tmp;
+    }
+    return ++result;
 }
 
 } // namespace Sstl

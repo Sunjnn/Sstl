51b7e3f085e6c5162343360f2db9b9e902849ab2
Sunjnn <sunjn990@gmail.com>
2023-07-09 15:29:39 +0800

    feat: add algorithm function `inner_product()`

--- Sstl/stl_numeric.h
+++ Sstl/stl_numeric.h
@@ -72,4 +72,27 @@ OutputIterator __adjacent_difference(InputIterator first, InputIterator last, Ou
 
 } // namespace Sstl
 
+// inner product
+namespace Sstl {
+
+// first version of two of inner product
+template<class InputIterator1, class InputIterator2, class T>
+T inner_product(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, T init) {
+    for (; first1 != last1; ++first1, ++first2)
+        init += *first1 * *first2;
+    return init;
+}
+
+// second version of two of inner product
+template<class InputIterator1, class InputIterator2, class T, class BinaryOperation1, class BinaryOperation2>
+T inner_product(InputIterator1 first1, InputIterator1 last1,
+                InputIterator2 first2, T init,
+                BinaryOperation1 binary_op1, BinaryOperation2 binary_op2) {
+    for (; first1 != last1; ++first1, ++first2)
+        init = binary_op1(init, binary_op2(*first1, *first2));
+    return init;
+}
+
+} // namespace Sstl inner product
+
 #endif

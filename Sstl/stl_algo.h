cc5cc2138b0aa4dae5fcb19bc692e0d12952c177
Sunjnn <sunjn990@gmail.com>
2023-08-09 22:46:28 +0800

    feat: add algorithm function `merge()`

--- Sstl/stl_algo.h
+++ Sstl/stl_algo.h
@@ -2,6 +2,7 @@
 #define __STL_ALGO_H__
 
 #include "stl_iterator.h"
+#include "stl_algobase.h"
 
 // find
 namespace Sstl {
@@ -295,6 +296,50 @@ ForwardIterator max_element(ForwardIterator first, ForwardIterator last,
 }
 
 } // namespace Sstl max_element
+
+// merge
+namespace Sstl {
+
+// first of two versions of merge
+template<class InputIterator1, class InputIterator2, class OutputIterator>
+OutputIterator merge(InputIterator1 first1, InputIterator1 last1,
+                     InputIterator2 first2, InputIterator2 last2,
+                     OutputIterator result) {
+    while (first1 != last1 && first2 != last2) {
+        if (*first2 < *first1) {
+            *result = *first2;
+            ++first2;
+        }
+        else {
+            *result = *first1;
+            ++first1;
+        }
+        ++result;
+    }
+    return copy(first2, last2, copy(first1, last1, result));
+}
+
+// second of two versions of merge
+template<class InputIterator1, class InputIterator2, class OutputIterator,
+         class Compare>
+OutputIterator merge(InputIterator1 first1, InputIterator1 last1,
+                     InputIterator2 first2, InputIterator2 last2,
+                     OutputIterator result, Compare comp) {
+    while (first1 != last1 && first2 != last2) {
+        if (comp(*first2, *first1)) {
+            *result = *first2;
+            ++first2;
+        }
+        else {
+            *result = *first1;
+            ++first1;
+        }
+        ++result;
+    }
+    return copy(first2, last2, copy(first1, last1, result));
+}
+
+} // namespace Sstl merge
 
 // count
 namespace Sstl {

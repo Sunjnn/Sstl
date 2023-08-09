e8fb8047f65d45d7ac9fb2d6bffd2982f96adcdf
Sunjnn <sunjn990@gmail.com>
2023-08-09 22:32:43 +0800

    feat: add algorithm function `include()`

--- Sstl/stl_algo.h
+++ Sstl/stl_algo.h
@@ -231,6 +231,45 @@ OutputIterator generate_n(OutputIterator first, Size n, Generator gen) {
 
 } // namespace Sstl generate_n
 
+// include
+namespace Sstl {
+// S1 and S2 are sorted
+// the number of a element in S1 is m and that in S2 is n
+// if m >= n, return true
+// else return false
+
+// first of two versions of include
+template<class InputIterator1, class InputIterator2>
+bool include(InputIterator1 first1, InputIterator1 last1,
+             InputIterator2 first2, InputIterator2 last2) {
+    while (first1 != last1 && first2 != last2) {
+        if (*first2 < *first1)
+            return false;
+        else if (*first1 < *first2)
+            ++first1;
+        else
+            ++first1, ++first2;
+    }
+    return first2 == last2;
+}
+
+// second of two version of include
+template<class InputIterator1, class InputIterator2, class Compare>
+bool include(InputIterator1 first1, InputIterator1 last1,
+             InputIterator2 first2, InputIterator2 last2, Compare comp) {
+    while (first1 != last1 && first2 != last2) {
+        if (comp(*first2, *first1))
+            return false;
+        else if (comp(*first1, *first2))
+            ++first1;
+        else
+            ++first1, ++first2;
+    }
+    return first2 == last2;
+}
+
+} // namespace Sstl include
+
 // count
 namespace Sstl {
 

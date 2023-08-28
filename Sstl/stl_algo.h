4474b71d1336327cba5eb6d82fd24dccd8c59dff
sunjnn <sunjn990@gmail.com>
2023-08-28 21:52:14 +0800

    feat: add algorithm function `lower_bound()`

--- Sstl/stl_algo.h
+++ Sstl/stl_algo.h
@@ -817,6 +817,68 @@ OutputIterator __unique_copy(InputIterator first, InputIterator last,
 
 } // namespace Sstl unique_copy
 
+// lower_bound
+namespace Sstl{
+
+template<class ForwardIterator, class T>
+inline ForwardIterator lower_bound(ForwardIterator first,
+                                   ForwardIterator last,
+                                   const T& value) {
+    return __lower_bound(first, last, value, distance_type(first),
+                         iterator_category(first));
+}
+
+template<class ForwardIterator, class T, class Distance>
+ForwardIterator __lower_bound(ForwardIterator first,
+                              ForwardIterator last,
+                              const T& value,
+                              Distance*,
+                              forward_iterator_tag) {
+    Distance len = 0;
+    distance(first, last, len);
+    Distance half;
+    ForwardIterator middle;
+
+    while (len > 0) {
+        half = len >> 1;
+        middle = first;
+        advance(middle, half);
+        if (*middle < value) {
+            first = middle;
+            ++first;
+            len = len - half - 1;
+        }
+        else
+            len = half;
+    }
+    return first;
+}
+
+template<class RandomAccessIterator, class T, class Distance>
+RandomAccessIterator __lower_bound(RandomAccessIterator first,
+                                   RandomAccessIterator last,
+                                   const T& value,
+                                   Distance*,
+                                   random_access_iterator_tag) {
+    Distance len = last - first;
+    Distance half;
+    RandomAccessIterator middle;
+
+    while (len > 0) {
+        half = len >> 1;
+        middle = first + half;
+        if (*middle < value) {
+            first = middle + 1;
+            len = len - half - 1;
+        }
+        else
+            len = half;
+    }
+    return first;
+}
+
+} // namespace Sstl lower_bound
+
 // merge
 namespace Sstl {
 

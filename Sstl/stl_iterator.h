219a1eb48c92036ca62b897ed745d9e28fea4730
sunjnn <sunjn990@gmail.com>
2023-09-28 10:35:46 +0800

    feat: add insert iterator adapters
    Include back_insert_iterator, front_insert_iterator, insert_iterator.

--- Sstl/stl_iterator.h
+++ Sstl/stl_iterator.h
@@ -131,4 +131,105 @@ inline void advance(InputIterator &i, Distance n) {
 
 }
 
+// insert iterator adapters
+namespace Sstl {
+
+// back insert iterator
+template<class Container>
+class back_insert_iterator {
+protected:
+    Container* container;
+public:
+    typedef output_iterator_tag iterator_category;
+    typedef void                value_type;
+    typedef void                difference_type;
+    typedef void                pointer;
+    typedef void                reference;
+
+    explicit back_insert_iterator(Container& x) : container(&x) {}
+
+    back_insert_iterator<Container>&
+    operator=(const typename Container::value_type& value) {
+        container->push_back(value);
+        return *this;
+    }
+
+    back_insert_iterator<Container>& operator*() { return *this; }
+    back_insert_iterator<Container>& operator++() { return *this; }
+    back_insert_iterator<Container>& operator++(int) { return *this; }
+};
+
+template<class Container>
+inline back_insert_iterator<Container> back_insert(Container& x) {
+    return back_insert_iterator<Container>(x);
+}
+
+
+// front insert iterator
+template<class Container>
+class front_insert_iterator {
+protected:
+    Container* container;
+public:
+    typedef output_iterator_tag iterator_category;
+    typedef void                value_type;
+    typedef void                difference_type;
+    typedef void                pointer;
+    typedef void                reference;
+
+    explicit front_insert_iterator(Container& x) : container(&x) {}
+
+    front_insert_iterator<Container>&
+    operator=(const typename Container::value_type& value) {
+        container->push_front(value);
+        return *this;
+    }
+
+    front_insert_iterator<Container>& operator*() { return *this; }
+    front_insert_iterator<Container>& operator++() { return *this; }
+    front_insert_iterator<Container>& operator++(int) { return *this; }
+};
+
+template<class Container>
+inline front_insert_iterator<Container> front_insert(Container& x) {
+    return front_insert_iterator<Container>(x);
+}
+
+
+// insert iterator
+template<class Container>
+class insert_iterator {
+protected:
+    Container* container;
+    typename Container::iterator iter;
+public:
+    typedef output_iterator_tag iterator_category;
+    typedef void                value_type;
+    typedef void                difference_type;
+    typedef void                pointer;
+    typedef void                reference;
+
+    insert_iterator(Container& x, typename Container::iterator i)
+        : container(&x), iter(i) {}
+
+    insert_iterator<Container>&
+    operator+(const typename Container::value_type& value) {
+        iter = container->insert(iter, value);
+        ++iter;
+        return *this;
+    }
+
+    insert_iterator<Container>& operator*() { return *this; }
+    insert_iterator<Container>& operator++() { return *this; }
+    insert_iterator<Container>& operator++(int) { return *this; }
+};
+
+template<class Container, class Iterator>
+inline insert_iterator<Container> inserter(Container& x, Iterator i) {
+    typedef typename Container::iterator iter;
+    return insert_iterator<Container>(x, iter(i));
+}
+
+} // namespace Sstl insert iterator adapters
+
 #endif
